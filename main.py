4c11d4e51ecacba68717db68f95ba9cbe09b6125
Kavin-nilaa <162492723+Kavin-nilaa@users.noreply.github.com>
2026-06-04 00:11:51 +0530

    Add number guessing game to main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Guess the number
+import random
+num = int(input("Guess the number"))
+r=random.randint(1, 100)
+print("System generated number is",str(r))
+if num == random:
+    print("Congratulations! You have guessed the number correct.")
+else:    
+    print("Better luck next time!") 
+
+#2. Word Scramble
